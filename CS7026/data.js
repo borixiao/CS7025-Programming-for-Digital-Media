a2793fb8967ee34935995890d680acc84ff2c474
POJUI HSIAO <48972641+borixiao@users.noreply.github.com>
2025-11-22 12:49:14 +0000

    Add files via upload
    This version added JavaScript function

---
+++ CS7026/data.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "Summer Vibes Festival",
+        description: "A branding and campaign design concept.",
+        img: "./img/w08JBQPFYIq2vr4OfcD9W6vxEug.avif"
+    },
+    {
+        title: "UI/UX Case Study",
+        description: "User-centred design with wireframes.",
+        img: "./img/nTU7b0ZAdWdlqCI4mQ4tGTPpDs.avif"
+    },
+    {
+        title: "CORAL SPIRAL ABSTRACT",
+        description: "A visually striking 3D abstract artwork featuring a coral-colored spiral",
+        img: "./img/qbjsnnvP9w7UaA2syp36oUe8OSo.avif"
+    },
+    {
+        title: "UI/UX Case Study",
+        description: "User-centred design with wireframes.",
+        img: "./img/2nWXrWvPxxMHSpsOkNYf8KjzP7Q.avif"
+    }
+];
